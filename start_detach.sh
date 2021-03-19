eda09e71b728237f73cfadf6a4d79dfd5192cd64
Aksel Stadler <aksel@stadler.no>
2021-03-19 11:28:04 +0100

    add extra telemtry

--- start_detach.sh
+++ start_detach.sh
@@ -1,9 +1,8 @@
 npm install --prefix assets
 npm run deploy --prefix assets
-mix phx.server
 
 lsof -i:4002 | grep beam | awk '{print $2}' | head -n 1 | xargs kill -9
-MIX_ENV=prod elixir --detached -S mix startup
+MIX_ENV=prod elixir --sname node  --detached -S mix startup
 
 
 
